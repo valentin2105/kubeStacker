cb5d107c0abfda4ff099a2c3d7094d91ff47858e
valentin2105 <valentin@ouvrard.it>
2016-12-16 17:31:44 +1100

    first commit

---
+++ commands.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/valentin2105/kubeStacker/command"
+)
+
+var GlobalFlags = []cli.Flag{}
+
+var Commands = []cli.Command{
+	{
+		Name:   "add",
+		Usage:  "",
+		Action: command.CmdAdd,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "show",
+		Usage:  "",
+		Action: command.CmdShow,
+		Flags:  []cli.Flag{},
+	},
+	{
+		Name:   "delete",
+		Usage:  "",
+		Action: command.CmdDelete,
+		Flags:  []cli.Flag{},
+	},
+}
+
+func CommandNotFound(c *cli.Context, command string) {
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	os.Exit(2)
+}
