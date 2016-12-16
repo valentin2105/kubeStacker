038e1a5abe67c17c2db666b942acdc803e695c4c
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2016-12-16 18:16:56 +1100

    lets go

--- commands.go
+++ commands.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-
 	"github.com/codegangsta/cli"
 	"github.com/valentin2105/kubeStacker/command"
 )
@@ -32,6 +31,6 @@ var Commands = []cli.Command{
 }
 
 func CommandNotFound(c *cli.Context, command string) {
-	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
+	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.\n ", c.App.Name, command, c.App.Name, c.App.Name)
 	os.Exit(2)
 }
